69a927477aeb4de04831191cb35b605904683ff6
Dor Kleiman <configurator@gmail.com>
2020-05-02 18:46:10 +0300

    Reorganize code

--- pkg/cgofusewrapper/file.go
+++ pkg/cgofusewrapper/file.go
@@ -1,11 +1,5 @@
 package cgofusewrapper
 
-import (
-	"log"
-
-	"github.com/configurator/kubefs/pkg/cgofusewrapper/errno"
-)
-
 type File interface {
 	ReadEntireContents() ([]byte, error)
 	Write([]byte) error
@@ -18,37 +12,4 @@ var _ Node = (*BaseFile)(nil)
 
 func (f *BaseFile) Attr(stat *Stat) (FileType, FilePermissions, error) {
 	return FileType_File, FilePermissions_Read, nil
-}
-
-// Read reads data from a file.
-func (fs *FS) Read(path string, buff []byte, offset int64, fh uint64) int {
-	log.Printf("fs.Read(%v, buffer, %#x, fh)\n", path, offset)
-
-	node, err := fs.findNode(path)
-	if err != nil {
-		return handleError(err)
-	}
-
-	file, ok := node.(File)
-	if !ok {
-		if _, ok := node.(Dir); ok {
-			return errno.EISDIR
-		}
-		return errno.EUNKNOWN
-	}
-
-	contents, err := file.ReadEntireContents()
-	if err != nil {
-		return handleError(err)
-	}
-
-	end := offset + int64(len(buff))
-	if end > int64(len(contents)) {
-		end = int64(len(contents))
-	}
-	if end < offset {
-		return 0
-	}
-
-	return copy(buff, contents[offset:end])
 }
--- pkg/cgofusewrapper/fs.go
+++ pkg/cgofusewrapper/fs.go
@@ -70,23 +70,3 @@ func (fs *FS) Getattr(path string, stat *fuse.Stat_t, fh uint64) int {
 	}
 	return 0
 }
-
-// Unlink removes a file.
-func (fs *FS) Unlink(path string) int {
-	log.Printf("fs.Unlink(%v)\n", path)
-
-	node, err := fs.findNode(path)
-	if err != nil {
-		return handleError(err)
-	}
-
-	if file, ok := node.(File); ok {
-		err := file.Delete()
-		if err != nil {
-			return handleError(err)
-		}
-		return 0
-	}
-
-	return errno.EOPNOTSUPP
-}
